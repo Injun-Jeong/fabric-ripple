88ff8f62bbd5aa28a8a5f09e88552d07a4408353
injun <dhak45@gmail.com>
2021-08-16 14:57:01 +0000

    feature: add [getWallet, transfer] api into application

---
+++ fabricRipple/application/server/api/fabric.sdk.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const { FileSystemWallet, Gateway } = require('fabric-network');
+const path = require('path');
+const ccpPath = path.resolve(__dirname, '../..', 'connection.json');
+
+
+/**
+ * 이름: send
+ * 설명: fabric network 체인코드 호출
+ * @param txInfo: 트랜잭션 설정 정보[invoke 호출여부, Wallet User ID, Channel ID, Chaincode name]
+ * @param func: 호출 체인코드 함수
+ * @param args: 호출 체인코드 인자
+ * @param res
+ */
+async function send(txInfo, func, args, res) {
+    try {
+        const type = txInfo[0];         // invoke 호출 여부
+        const walletUser = txInfo[1];   // Wallet User ID
+        const channelId = txInfo[2];    // Channel ID
+        const chaincode = txInfo[3];    // Chaincode name
+
+        const walletPath = path.join(process.cwd(), '..', 'wallet');
+        const wallet = new FileSystemWallet(walletPath);
+        console.log(`Wallet path: ${walletPath}`);
+
+        const userExists = await wallet.exists(walletUser);
+        if (!userExists) {
+            console.log(`An identity for the user ${walletUser} does not exist in the wallet`);
+            console.log('Run the registerUser.js application before retrying');
+            return ;
+        }
+
+        const gateway = new Gateway();
+        await gateway.connect(ccpPath, { wallet, identity: walletUser, discovery: { enabled: true, asLocalhost: true } });
+        const network = await gateway.getNetwork(channelId);
+        const contract = network.getContract(chaincode);
+
+        if (type) {
+            // invoke 함수 호출
+            await contract.submitTransaction(func, ...args);
+            console.log('Transaction has been submitted');
+            await gateway.disconnect();
+            res.send('Transaction has been submitted');
+        } else {
+            // query 함수 호출
+            const result = await contract.evaluateTransaction(func, ...args);
+            console.log(`Transaction has been evaluated, result is: ${result.toString()}`);
+            res.send(result.toString());
+        }
+
+    } catch(error) {
+        console.error(`Failed to submit transaction: ${error}`);
+        res.send(`Failed to submit transaction: ${error}`);
+    }
+}
+
+module.exports = { send }
--- fabricRipple/application/server/api/wallet/wallet.ctrl.js
+++ fabricRipple/application/server/api/wallet/wallet.ctrl.js
@@ -1,28 +1,64 @@
 const QRcode = require('qrcode');
+const fabricSdk = require('../fabric.sdk');
 
 /**
- * 이름:
- * 설명:
+ * 이름: index
+ * 설명: 지갑 정보 조회
  * 입력
- *  -
+ *  - account       계좌번호
+ *  - walletUser    지갑 사용자
+ *  - channelId     채널 ID
+ *  - chaincode     체인코드 이름
  * 출력
  *  -
  */
 const index = function(req, res) {
-    return res.send("");
+    const account = req.query.account;
+    let args = [account];
+
+    /* todo: transaction info 설정 방법 */
+    const invokeYn = false;
+    const walletUser = req.query.walletUser;
+    const channelId = req.query.channelId;      // { channelkorea, channelusa }
+    const chaincode = req.query.chaincode;
+    let txInfo = [invokeYn, walletUser, channelId, chaincode]
+
+    // 지갑 정보 조회 체인코드 호출
+    fabricSdk.send(txInfo, 'getWallet', args, res);
 }
 
+
+
 /**
- * 이름:
- * 설명:
+ * 이름: transfer
+ * 설명: 송금
  * 입력
- *  -
+ *  - account       계좌번호
+ *  - destination   수신 계좌번호
+ *  - amount        금액
+ *  - walletUser    지갑 사용자
+ *  - channelId     채널 ID
+ *  - chaincode     체인코드 이름
  * 출력
  *  -
  */
 const transfer = function(req, res) {
-    return res.send("");
+    const account = req.query.account;
+    const destination = req.query.destination;
+    const amount = req.query.amount;
+    let args = [account, destination, amount];
+
+    /* todo: transaction info 설정 방법 */
+    const invokeYn = true;
+    const walletUser = req.query.walletUser;
+    const channelId = req.query.channelId;      // { channelkorea, channelusa }
+    const chaincode = req.query.chaincode;
+    let txInfo = [invokeYn, walletUser, channelId, chaincode]
+
+    // 송금 체인코드 호출
+    fabricSdk.send(txInfo, 'transfer', args, res);
 }
+
 
 
 /**
